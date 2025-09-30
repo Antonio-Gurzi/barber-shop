59ddad1fd9ea0bf7f85a6833891fc19435f2fcc8
Antonio Gurzì <antonio.gurzi9@gmail.com>
2025-09-30 02:12:33 +0200

    lavorato su orari e giorni appuntamenti

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -21,22 +21,76 @@ class AppointmentController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
+        // orari negozio
+        $hours = [
+            '09:00',
+            '10:00',
+            '11:00',
+            '12:00',
+            '15:00',
+            '16:00',
+            '17:00',
+            '18:00',
+        ];
+
+
+        $appointments = Appointment::all();
+
+        //calcolo durata del servizio
+        foreach ($appointments as $appt) {
+            // Accede alla proprietà duration della tabella services
+            $duration = ($appt->service->duration);
+        }
+        //prendo il giorno selezionato a calendario
+        $selectDay = Appointment::pluck('day')->toArray();
+
+
+        // Estrai mese e giorno dal campo day
+        // $monthDay = substr($daySelected, 5, 5); // MM-DD
+        // dd($monthDay);
+        //prendo la colonna time dell appuntamento
+
+        $times = Appointment::pluck('time')->toArray();
+
+        //adeguo i formati e sommo l'appuntamento preso alla durata del servizio selezionato
+        foreach ($times as $time) {
+            // Prendi solo ore e minuti se il formato è HH:MM:SS
+            $timeHM = substr($time, 0, 5);
+            $timeCarbon = Carbon::createFromFormat('H:i', $timeHM);
+            $newTime = $timeCarbon->addMinutes($duration)->format('H:i');
+
+
+            // Controlla se è presente e rimuovi
+            if (in_array($timeHM, $hours)) {
+                $hourOccupated = array_search($timeHM, $hours);
+                unset($hours[$hourOccupated]);
+                //restituisce l array con i nuovi orari
+                $hours[] = $newTime;
+                //ordine l array in ordine crescente
+                sort($hours);
+            }
+        }
+
+        //riaggiorno l array aggiungendo un nuovo orario che parte dalla fine dell apputamento precedente(refreshHour e ricontrollo tutti gli orari,se già esistono,cancello quelli occupati)
+        $refhreshTime = Appointment::pluck('time')->toArray();
+
+        if (in_array($refhreshTime && $timeHM, $hours)) {
+            $hourOccupated = array_search($refhreshTime && $timeHM, $hours);
+            unset($hours[$hourOccupated]);
+        }
+
+        // Reindicizza l'array
+        $hours = array_values($hours);
+
         $user = Auth::user(); // utente loggato
         $services = Service::all(); // tutti i servizi
-        // orari
-        $hours = [];
-        for ($h = 9; $h < 13; $h++) {
-            foreach ([0, 30] as $m) {
-                $hours[] = sprintf("%02d:%02d", $h, $m);
-            }
-        }
-        for ($h = 15; $h < 18; $h++) {
-            foreach ([0, 30] as $m) {
-                $hours[] = sprintf("%02d:%02d", $h, $m);
-            }
-        }
+
+
+
+
+
         return view('appointment/create', compact('user', 'services', 'hours'));
     }
 
@@ -53,7 +107,7 @@ class AppointmentController extends Controller
         $appointment->service_id = $request->service_id;
 
         // Se vuoi salvare l’utente loggato
-        $appointment->user_id = auth()->id();
+        $appointment->user_id = Auth::id();
 
         // Salva il record
         $appointment->save();
